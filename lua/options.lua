95b3feadada033dddd8937d4e3c3cc98aaccc388
Ryoya Otomo <s2212214@st.tohtech.ac.jp>
2025-04-17 16:26:28 +0900

    設定途中

---
+++ lua/options.lua
@@ -0,0 +1,26 @@
+local vim = vim.opt
+
+-- 行番号表示
+vim.number = true
+vim.relativenumber = false
+
+-- タブ・インデント設定
+vim.tabstop = 4
+vim.shiftwidth = 4
+vim.expandtab = true
+
+-- 検索設定
+vim.smartcase = true
+vim.ignorecase = false
+vim.hlsearch = true
+vim.incsearch = true
+
+-- 表示設定
+vim.wrap = false
+vim.linebreak = false
+vim.cursorline = true
+vim.showcmd = true
+
+-- ファイル設定
+
+-- その他の設定
